5028f06e116e947dc411eb27e4efd563acff48dc
Alexander <en.alex94@gmail.com>
2023-01-18 12:10:03 +0300

    Initial Commit

---
+++ Drinkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drinkit
+//
+//  Created by Александр Николаевич on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
